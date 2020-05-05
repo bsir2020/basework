d0f8b06b8fd1fa4a8f01c1219714b037fd1d8481
bsir2020 <b5496@outlook.com>
2020-05-05 17:49:36 +0800

    update

--- pkg/datasource/redis.go
+++ pkg/datasource/redis.go
@@ -5,7 +5,6 @@ import (
 	cfg "github.com/bsir2020/basework/configs"
 	"github.com/bsir2020/basework/pkg/log"
 	"github.com/garyburd/redigo/redis"
-	"go.uber.org/zap"
 	"time"
 )
 
@@ -33,12 +32,12 @@ func newRedisPool() *redis.Pool {
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.DialURL(redisURL)
 			if err != nil {
-				loger(logger, "redis connection error", err)
+				logger.Fatal("newRedisPool", "redis connection error", err)
 				return nil, fmt.Errorf("redis connection error: %s", err)
 			}
 			//验证redis密码
 			if _, authErr := c.Do("AUTH", redisPassword); authErr != nil {
-				loger(logger, "redis auth password error", err)
+				logger.Fatal("newRedisPool", "redis connection error", err)
 
 				return nil, fmt.Errorf("redis auth password error: %s", authErr)
 			}
@@ -47,7 +46,7 @@ func newRedisPool() *redis.Pool {
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			_, err := c.Do("PING")
 			if err != nil {
-				loger(logger, "ping redis error", err)
+				logger.Fatal("newRedisPool", "redis connection error", err)
 
 				return fmt.Errorf("ping redis error: %s", err)
 			}
@@ -58,8 +57,4 @@ func newRedisPool() *redis.Pool {
 
 func GetRedisConn() redis.Conn {
 	return newRedisPool().Get()
-}
-
-func loger(logger *zap.Logger, msg string, err error) {
-	logger.Fatal(msg, zap.String("err", err.Error()))
 }
