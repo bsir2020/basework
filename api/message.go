e27c3858d17785ffba96b3853e63874e9ee0e403
bsir2020 <b5496@outlook.com>
2020-05-17 12:09:03 +0800

    interface -> []byte

--- api/message.go
+++ api/message.go
@@ -1,12 +1,12 @@
 package api
 
 type Message struct {
-	Item    string      `json:"item"`
-	Subject string      `json:"subject"`
-	Mtype   int         `json:"type"` //#0发起 1反馈
-	Status  bool        `json:"status"`
-	Id      int64       `json:"id"`
-	Data    interface{} `json:"data"`
+	Item    string `json:"item"`
+	Subject string `json:"subject"`
+	Mtype   int    `json:"type"` //#0发起 1反馈
+	Status  bool   `json:"status"`
+	Id      int64  `json:"id"`
+	Data    []byte `json:"data"`
 }
 
 // 定义错误码
